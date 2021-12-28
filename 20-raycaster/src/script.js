b1ff469462b6460ac8c7dc87743509b32666c21f
Szymon Rut <rut.szymon@gmail.com>
2021-12-28 16:16:13 +0100

    20-raycaster mouse hover

--- 20-raycaster/src/script.js
+++ 20-raycaster/src/script.js
@@ -66,6 +66,16 @@ window.addEventListener('resize', () =>
 })
 
 /**
+ * Mouse
+ */
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.clientX / sizes.width * 2 - 1
+    mouse.y = - (event.clientY / sizes.height) * 2 + 1
+})
+
+/**
  * Camera
  */
 // Base camera
@@ -101,11 +111,7 @@ const tick = () =>
     object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
 
     // Cast a ray
-    const rayOrigin = new THREE.Vector3(- 3, 0, 0)
-    const rayDirection = new THREE.Vector3(1, 0, 0)
-    rayDirection.normalize()
-
-    raycaster.set(rayOrigin, rayDirection)
+    raycaster.setFromCamera(mouse, camera)
 
     const objectsToTest = [object1, object2, object3]
     const intersects = raycaster.intersectObjects(objectsToTest)
