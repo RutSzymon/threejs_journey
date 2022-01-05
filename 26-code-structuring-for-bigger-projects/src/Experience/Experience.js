db69fba2e2dd8a161de12eee25a8e00ae944bcbf
Szymon Rut <rut.szymon@gmail.com>
2022-01-05 12:20:12 +0100

    26 Time

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,4 +1,5 @@
 import Sizes from './Utils/Sizes.js'
+import Time from './Utils/Time.js'
 
 export default class Experience {
   constructor(canvas) {
@@ -10,13 +11,24 @@ export default class Experience {
 
     // Setup
     this.sizes = new Sizes()
+    this.time = new Time()
 
+    // Sizes resize event
     this.sizes.on('resize', () => {
       this.resize()
+    })
+
+    // Time tick event
+    this.time.on('tick', () => {
+      this.update()
     })
   }
 
   resize() {
     console.log('A resize occured')
+  }
+
+  update() {
+
   }
 }
