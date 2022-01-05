cc0892b55c9a135e599f7b2fc2b2670d10bcf4d7
Szymon Rut <rut.szymon@gmail.com>
2022-01-05 12:34:04 +0100

    26 Camera

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,9 +1,18 @@
 import * as THREE from 'three'
 import Sizes from './Utils/Sizes.js'
 import Time from './Utils/Time.js'
+import Camera from './Camera.js'
+
+let instance = null
 
 export default class Experience {
   constructor(canvas) {
+    if (instance) {
+      return instance
+    }
+
+    instance = this
+
     // Global access
     window.experience = this
 
@@ -14,6 +23,7 @@ export default class Experience {
     this.sizes = new Sizes()
     this.time = new Time()
     this.scene = new THREE.Scene()
+    this.camera = new Camera()
 
     // Sizes resize event
     this.sizes.on('resize', () => {
