232c428db8ea7c3d49978b214df34208c5305a38
Szymon Rut <rut.szymon@gmail.com>
2022-01-05 11:24:53 +0100

    26 classes in separate files

--- 26-code-structuring-for-bigger-projects/src/script.js
+++ 26-code-structuring-for-bigger-projects/src/script.js
@@ -1,35 +1,5 @@
-class Robot {
-    constructor(name, legs) {
-        this.name = name
-        this.legs = legs
-
-        console.log(`I am ${this.name}. Thank you creator`)
-    }
-
-    sayHi() {
-        console.log(`Hello! My name is ${this.name}`)
-    }
-}
-
-class FlyingRobot extends Robot {
-    constructor(name, legs) {
-        super(name, legs)
-
-        super.sayHi()
-    }
-
-    sayHi() {
-        console.log(`Hello! My name is ${this.name} and I am a flying robot`)
-    }
-
-    takeOff() {
-        console.log(`Have a good flight ${this.name}`)
-    }
-
-    land() {
-        console.log(`Welcome back ${this.name}`)
-    }
-}
+import Robot from './Robot.js'
+import FlyingRobot from './FlyingRobot.js'
 
 const wallE = new Robot('Wall-E', 0)
 const ultron = new FlyingRobot('Ultron', 2)
