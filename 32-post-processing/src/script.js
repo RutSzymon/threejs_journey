051ae96e6a79f862cbfee9fc7ac598c1eb530719
Szymon Rut <rut.szymon@gmail.com>
2022-02-23 16:39:52 +0100

    32-post-processing final

--- 32-post-processing/src/script.js
+++ 32-post-processing/src/script.js
@@ -251,7 +251,7 @@ gui.add(tintPass.material.uniforms.uTint.value, 'z').min(- 1).max(1).step(0.001)
 const DisplacementShader = {
     uniforms: {
         tDiffuse: { value: null },
-        uTime: { value: null }
+        uNormalMap: { value: null }
     },
     vertexShader: `
         varying vec2 vUv;
@@ -264,22 +264,25 @@ const DisplacementShader = {
     `,
     fragmentShader: `
         uniform sampler2D tDiffuse;
-        uniform float uTime;
+        uniform sampler2D uNormalMap;
 
         varying vec2 vUv;
 
         void main() {
-            vec2 newUv = vec2(
-                vUv.x,
-                vUv.y + sin(vUv.x * 10.0 + uTime) * 0.1
-            );
+            vec3 normalColor = texture2D(uNormalMap, vUv).xyz * 2.0 - 1.0;
+            vec2 newUv = vUv + normalColor.xy * 0.1;
             vec4 color = texture2D(tDiffuse, newUv);
+
+            vec3 lightDirection = normalize(vec3(- 1.0, 1.0, 0.0));
+            float lightness = clamp(dot(normalColor, lightDirection), 0.0, 1.0);
+            color.rgb += lightness * 2.0;
+
             gl_FragColor = color;
         }
     `
 }
 const displacementPass = new ShaderPass(DisplacementShader)
-displacementPass.material.uniforms.uTime.value = 0
+displacementPass.material.uniforms.uNormalMap.value = textureLoader.load('/textures/interfaceNormalMap.png')
 effectComposer.addPass(displacementPass)
 
 // SMAA pass
@@ -299,9 +302,6 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
-    // Update passes
-    displacementPass.material.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
