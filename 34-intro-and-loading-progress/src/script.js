6e3654e6e3d379b4649ee3c4d746687107c062dc
Szymon Rut <rut.szymon@gmail.com>
2022-03-14 15:41:29 +0100

    34 loading bar

--- 34-intro-and-loading-progress/src/script.js
+++ 34-intro-and-loading-progress/src/script.js
@@ -7,9 +7,14 @@ import { gsap } from 'gsap'
 /**
  * Loaders
  */
+const loadingBarElement = document.querySelector('.loading-bar')
+
 const loadingManager = new THREE.LoadingManager(
     () => { gsap.to(overlayMaterial.uniforms.uAlpha, { duration: 3, value: 0 }) },
-    () => { console.log('progress') }
+    (itemUrl, itemsLoaded, itemsTotal) => {
+        const progressRatio = itemsLoaded / itemsTotal
+        loadingBarElement.style.transform = `scalex(${progressRatio})`
+    }
 )
 const gltfLoader = new GLTFLoader(loadingManager)
 const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager)
