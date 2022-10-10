739675bedbf13affa8471580c7c95e43462811d1
Szymon Rut <rut.szymon@gmail.com>
2022-10-10 15:34:42 +0200

    38 loading the texture

--- 38-importing-and-optimizing-the-scene/src/script.js
+++ 38-importing-and-optimizing-the-scene/src/script.js
@@ -34,10 +34,16 @@ const gltfLoader = new GLTFLoader()
 gltfLoader.setDRACOLoader(dracoLoader)
 
 /**
+ * Textures
+ */
+const bakedTexture = textureLoader.load('baked.jpg')
+bakedTexture.flipY = false
+
+/**
  * Materials
  */
 // Baked material
-const bakedMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const bakedMaterial = new THREE.MeshBasicMaterial({ map: bakedTexture })
 
 /**
  * Model
